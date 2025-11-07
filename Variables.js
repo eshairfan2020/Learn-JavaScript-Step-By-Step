401d7e69b684be096d432c2a82d9c35862d32d53
eshairfan2020 <eeshairfan2020@gmail.com>
2025-11-07 23:49:49 +0500

    Add Variables.js with variable declarations

---
+++ Variables.js
@@ -0,0 +1,8 @@
+// Variables in JavaScript
+var oldWay = "I am var (avoid me)";
+let name = "Alice"; // can change
+const age = 25;     // cannot change
+
+console.log("Old variable:", oldWay);
+console.log("Name:", name);
+console.log("Age:", age);
